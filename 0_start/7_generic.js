a164f9999df2671cbab9b1b3c507d9052241e03d
ivanstepanov <ivanstepanoff@yahoo.com>
2021-05-23 09:53:16 +0200

    branch react-ts-todos is created

--- 0_start/7_generic.js
+++
@@ -1,8 +0,0 @@
-var arrayOfNumbers = [1, 2, 3];
-var arrayOfStrings = ['Hello', 'Vlad'];
-function reverse(array) {
-    return array.reverse();
-}
-reverse(arrayOfStrings);
-reverse(arrayOfNumbers);
-// них не понял, есть any типы и зачем к имени функции <T> если в параметрах указываем работу с разными типами...
