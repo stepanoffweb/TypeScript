f5415ae0b6fbced4ad1ecfb62aa2b03e42f8f90d
ivanstepanov <ivanstepanoff@yahoo.com>
2021-06-08 07:56:16 +0200

    C:/Program Files/Git/e_r_ts was deleted

--- 3_r-ts/src/components/ListItems.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react"
-interface ListItemsProps<T> {
-  items: T[]
-  renderItem: (item: T) => React.ReactNode
-}
-
-export default function ListItems<T>(props: ListItemsProps<T>) {
-  return <ul>{props.items.map(item => props.renderItem(item))}</ul>
-}
