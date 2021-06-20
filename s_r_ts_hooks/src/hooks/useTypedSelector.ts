efa6791b70225d24764a2b8388034255da729ffb
ivanstepanov <ivanstepanoff@yahoo.com>
2021-06-20 08:37:33 +0200

    C:/Program Files/Git/s_r_ts_hooks was deleted

--- s_r_ts_hooks/src/hooks/useTypedSelector.ts
+++
@@ -1,4 +0,0 @@
-import { TypedUseSelectorHook, useSelector } from "react-redux"
-import { RootReducer } from "../store/reducers"
-
-export const useTypedSelector: TypedUseSelectorHook<RootReducer> = useSelector
--- s_r_ts_hooks/src/store/action-creators/index.ts
+++
@@ -1,7 +0,0 @@
-import * as TodosActionCreators from "./todos"
-import * as UsersActionCreators from "./users"
-
-export default {
-  ...TodosActionCreators,
-  ...UsersActionCreators,
-}
